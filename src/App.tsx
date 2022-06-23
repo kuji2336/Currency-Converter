30af0de9a869fd839524e066955468a21be030fb
kujx <quji.bolqvadze123@gmail.com>
2022-06-23 23:07:14 +0400

    fix:fixed prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import "./global/css/global.css";
 import useGetDefaultCurrency from "./global/hooks/useGetDefaultCurrency";
 
 const Converter = React.lazy(() =>
-    import(/*webpackChunkName: “converter-page” */ "./view/convert/index").then((module) => ({
+    import(/*webpackChunkName: "converter-page" */ "./view/convert/index").then((module) => ({
         default: module.Converter
     }))
 );
 
 const LiveRates = React.lazy(() =>
-    import(/*webpackChunkName: “live-rates-page” */ "./view/live-rates").then((module) => ({
+    import(/*webpackChunkName: "live-rates-page" */ "./view/live-rates").then((module) => ({
         default: module.LiveRates
     }))
 );
